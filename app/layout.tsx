2419f83a35a3193c98788bf2b01133953c0815fc
CaffeinatedCheetah <babacar.diop529@gmail.com>
2026-07-24 13:26:51 -0400

    feat: wire RailProvider + ShellGate into root layout for site-wide shell

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Footer } from "@/components/navigation/footer";
 import { Navbar } from "@/components/navigation/navbar";
+import { RailProvider } from "@/components/navigation/rail-context";
+import { ShellGate } from "@/components/navigation/shell-gate";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -19,11 +21,16 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full antialiased">
       <body className="flex min-h-full flex-col">
-        <Navbar />
-        <main id="main-content" className="flex flex-1 flex-col pb-16 lg:pb-0">
-          {children}
-        </main>
-        <Footer />
+        <RailProvider>
+          <Navbar />
+          <main
+            id="main-content"
+            className="flex flex-1 flex-col pb-16 lg:pb-0"
+          >
+            <ShellGate>{children}</ShellGate>
+          </main>
+          <Footer />
+        </RailProvider>
       </body>
     </html>
   );
